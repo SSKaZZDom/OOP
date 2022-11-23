fe82b8adcbb7896edefa4288d35443d29e704b40
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2022-11-23 20:18:28 +0700

    without creating a file

--- Task_1_3_1/src/test/java/count/substring/SubstringTests.java
+++ Task_1_3_1/src/test/java/count/substring/SubstringTests.java
@@ -16,46 +16,34 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SubstringTests {
     @Test
     public void testUnitSearch() {
-        try (RandomAccessFile f = new RandomAccessFile("./src/test/resources/input1.txt", "rw")) {
-            f.setLength(10);
-            f.writeBytes("pppppie");
-            String sub = "pie";
-            List<Long> expect = new ArrayList<>();
-            expect.add(4L);
-            List<Long> result = new ArrayList<>();
-            InputStream stream = getClass().getClassLoader().getResourceAsStream("input1.txt");
-            try {
-                result.addAll(allEntries(stream, sub));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            Assertions.assertEquals(result, expect);
-        } catch (Exception c) {
-            fail();
+        String sub = "pie";
+        List<Long> expect = new ArrayList<>();
+        expect.add(4L);
+        List<Long> result = new ArrayList<>();
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("input1.txt");
+        try {
+            result.addAll(allEntries(stream, sub));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        Assertions.assertEquals(result, expect);
     }
 
     @Test
     public void testSetOfIndexes() {
-        try (RandomAccessFile f = new RandomAccessFile("./src/test/resources/input2.txt", "rw")) {
-            f.setLength(20);
-            f.writeBytes("pie piepie");
-            String sub = "pie";
-            List<Long> expect = new ArrayList<>();
-            expect.add(0L);
-            expect.add(4L);
-            expect.add(7L);
-            InputStream stream = getClass().getClassLoader().getResourceAsStream("input2.txt");
-            List<Long> result = null;
-            try {
-                result = new ArrayList<>(allEntries(stream, sub));
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-            Assertions.assertEquals(expect, result);
-        } catch (Exception c) {
-            fail();
+        String sub = "pie";
+        List<Long> expect = new ArrayList<>();
+        expect.add(0L);
+        expect.add(4L);
+        expect.add(7L);
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("input2.txt");
+        List<Long> result = null;
+        try {
+            result = new ArrayList<>(allEntries(stream, sub));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+        Assertions.assertEquals(expect, result);
     }
 
     @Test
@@ -75,24 +63,18 @@ public class SubstringTests {
 
     @Test
     public void testHugeFile() {
-        try (RandomAccessFile f = new RandomAccessFile("./src/test/resources/giant.txt", "rw")) {
-            f.setLength(20000000L);
-            f.seek(2500);
-            f.writeBytes("haha");
-            f.seek(5000);
-            f.writeBytes("haha");
-            f.seek(2576000);
-            f.writeBytes("haha");
-            f.seek(0);
-            InputStream stream = getClass().getClassLoader().getResourceAsStream("giant.txt");
-            List<Long> expect = new ArrayList<>();
-            expect.add(2500L);
-            expect.add(5000L);
-            expect.add(2576000L);
-            List<Long> result = new ArrayList<>(allEntries(stream, "haha"));
-            Assertions.assertEquals(expect, result);
-        } catch (Exception c) {
-            fail();
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("giant.txt");
+        List<Long> expect = new ArrayList<>();
+        expect.add(2500L);
+        expect.add(5000L);
+        expect.add(2576000L);
+        String sub = "haha";
+        List<Long> result = null;
+        try {
+            result = new ArrayList<>(allEntries(stream, sub));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+            Assertions.assertEquals(expect, result);
     }
 }
