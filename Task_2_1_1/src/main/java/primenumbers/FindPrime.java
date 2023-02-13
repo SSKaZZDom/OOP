435a7b68465294f0bb501ae4fbf261e598a7edee
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2023-02-13 14:16:33 +0700

    Soft deadline

---
+++ Task_2_1_1/src/main/java/primenumbers/FindPrime.java
@@ -0,0 +1,40 @@
+package primenumbers;
+
+import java.util.List;
+
+import static java.lang.Math.sqrt;
+
+public class FindPrime extends Thread {
+    List<Integer> nums;
+    boolean res;
+    public FindPrime(List<Integer> nums) {
+        this.nums = nums;
+    }
+
+    public boolean doNotHavePrime() {
+        for (Integer o : nums){
+            if (isNotPrime(o)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isNotPrime(int num) {
+        for (int i = 2; i <= sqrt(num); i++){
+            if (num % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean getResult() {
+        return res;
+    }
+
+    @Override
+    public void run() {
+        res = doNotHavePrime();
+    }
+}
