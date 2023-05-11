593c5c94a35b1fdd436749f8e8240021174bc09b
Vlad Lomakin <v.lomakin1@g.nsu.ru>
2023-05-11 09:30:25 +0700

    Rewrite Pizza without record

--- Task_2_2_1/src/main/java/pizzeria/Pizza.java
+++ Task_2_2_1/src/main/java/pizzeria/Pizza.java
@@ -1,4 +1,25 @@
 package pizzeria;
 
-public record Pizza(int timeToCooking, int range, int orderId) {
+public class Pizza {
+    private final int timeToCooking;
+    private final int range;
+    private final int orderId;
+
+    public Pizza(int timeToCooking, int range, int orderId) {
+        this.timeToCooking = timeToCooking;
+        this.range = range;
+        this.orderId = orderId;
+    }
+
+    public int timeToCooking() {
+        return timeToCooking;
+    }
+
+    public int range() {
+        return range;
+    }
+
+    public int orderId() {
+        return orderId;
+    }
 }
